c43af05cc7d3105f337ef4909b4c7cd9d519a5f5
ah <ah@gmail.com>
2022-06-22 11:08:12 +0800

    feat: add sidebar

---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,85 @@
+module.exports = {
+  "/awesome/sub/": [
+    {
+      "text": "Sub",
+      "title": "Sub",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "test.md"
+      ]
+    }
+  ],
+  "/awesome/": [
+    {
+      "text": "Awesome",
+      "title": "Awesome",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "Setting-Appearances.md",
+        "add-push-git-idea.md",
+        "auto-import-idea.md",
+        "background-setting-idea.md",
+        "base.md",
+        "branch-git-idea.md",
+        "breakpointer-debugger.md",
+        "cant-remember-passpord-datagrid.md",
+        "cn-vim-vscode-resolve.md",
+        "color-font-config-idea.md",
+        "commit-git-idea.md",
+        "config-interface-idea.md",
+        "debugger.md",
+        "debugger-tool-idea.md",
+        "edit-nav-idea.md",
+        "edit-tab-idea.md",
+        "first-java-idea.md",
+        "git-flow-idea.md",
+        "git-git-idea.md",
+        "git-idea.md",
+        "index.md",
+        "install-idea.md",
+        "interface-idea.md",
+        "kerboard.md",
+        "light-edit-idea.md",
+        "log-git-idea.md",
+        "menu-toolbar-idea.md",
+        "menus-and-tools.md",
+        "merge-idea.md",
+        "multiple-point-idea.md",
+        "multiple-pointer-idea-v1.md",
+        "no-crack-idea.md",
+        "plugins.md",
+        "pre.md",
+        "pre-idea.md",
+        "quick-list.md",
+        "rebase-idea.md",
+        "revert history commit in「IntelliJ IDEA」.md",
+        "search-replace-idea.md",
+        "show-model-idea.md",
+        "step-to-step-breakpointer-debugger-idea.md",
+        "theme-idea.md",
+        "update-theme-idea.md",
+        "version.md",
+        "vim-idea.md",
+        "vim-mac-idea.md",
+        "window-debugger-idea.md",
+        "windows-mac-keyboard-compare-idea.md",
+        "zhihu-29076145.md"
+      ]
+    }
+  ],
+  "/plugins/": [
+    {
+      "text": "Plugins",
+      "title": "Plugins",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ]
+};
